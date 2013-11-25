251b0cd224c3ed911f94a8b23fdcba44ecec6e70
mpn3712 <mpn3712@rit.edu>
2013-11-24 21:04:09 -0500

    added character reading

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 import re as regex
 import nltk
 import linecache
+from sets import Set
+from string import punctuation
 
 class Character:
-	def __init__(self, name, line, aliases=[]):
+
+	def __init__(self, name, aliases=[]):
 		self.name = name
 		self.aliases = aliases
-		self.line = line
-		self.character_line = []
-		self.aliases = []
+		self.lines = []
+		self.aliases = Set()
 
 	def get_sentiment():
 		return 100
 
-	#TODO
-	def check_similar(self, name):
-		self_name = self.name
-		if len(name) > len(self.name):
-			self.name = name
-			self.aliases.append(self_name)
-		else:
-			
 
 class PlayVis:
+
 	def __init__(self, filename, outfile):
 		self.filename = filename
 		self.outfile = outfile
-		self.format_paper()
 		self.characters = []
+		self.character_lines = []
+		self.format_paper()
 
 	def format_paper(self):
 		save = '';
-		readfile = open(filename)
+		readfile = open(self.filename)
 		outfile = open(self.outfile, 'w')
 
 		character_line = False
+		ii = 0
+
+		#print self.__dict__
 		for line in readfile:
 			# looks for the name
-			matches = regex.search('^\s\s+[a-zA-Z]+\s?[a-zA-Z]+', line)
+			matches = regex.search('^\s\s[a-zA-Z]+\s?[a-zA-Z]+\.', line)
 			# if it found a name
-			if not matches == None: 
+			if matches is not None:
+				self.character_lines.append(ii)
 				# we are in a characters line
 				character_line = True
 			elif line[0] == ' ':
@@ -51,16 +51,31 @@ class PlayVis:
 			else:
 				character_line = False # we are not in a character line
 				save += '************************************\n' # show that
-
+			ii += 1
 
 		outfile.write(save)
 
 	#TODO
-	def find_characters():
-		for line_num in self.character_line:
-			line = linecache.getline(self.outfile, line_num);			
-			
-			
+	def find_characters(self):
+		#print self.character_lines
+		for line_num in self.character_lines:
+			changed = False
+			line = linecache.getline(self.filename, line_num + 1);
+			matches = regex.search('^\s\s[a-zA-Z]+\s?[a-zA-Z]+\.', line)
+			name = matches.group()
+			punct_replace = regex.compile('[%s]' % regex.escape(punctuation))
+			name = punct_replace.sub('', name)
+			name = name.strip()
+			for character in self.characters:
+				if name in character.name:
+					character.aliases.add(name)
+					character.lines.append(line_num)
+					changed = True
+					break
+			if not changed:
+				self.characters.append(Character(name))
 
-pv = PlayVis('texts/hamlet.txt', 'output.txt')
-pv.find_characters();
+pv = PlayVis('texts/julius_ceasar.txt', 'output.txt')
+pv.find_characters()
+for character in pv.characters:
+	print character.__dict__
