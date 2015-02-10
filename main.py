502bdb2c768af1fd4f4ec9f15e370e09ada536a2
Mike Nolan <me@michael-nolan.com>
2015-02-10 11:42:33 -0500

    pep8 compliant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import re as regex
-import nltk
 import linecache
 import enchant
 import json
@@ -7,28 +6,32 @@ import sys
 from sets import Set
 from string import punctuation
 
+# create English dictionary
+d = enchant.Dict("en_US")
 
-d = enchant.Dict("en_US") #create english dictionary
-
-sDict = {} # create dictionary of sentiment words
+# create dictionary of sentiment words
+sDict = {}
 sent = open('sentiment/sentiment.txt')
+
 for line in sent:
     line = line.split()
-    #print line
+    # print line
     key = line[0]
     value = int(line[-1])
     sDict[key] = value
 
-sDict2 = {} # create dictionary of sentiment words
+# create dictionary of sentiment words
+sDict2 = {}
 sent = open('sentiment/lsd.txt')
 for line in sent:
     line = line.lower()
-    line = line.translate(None,'*')
+    line = line.translate(None, '*')
     line = line.split()
     if len(line) != 0:
         key = line[0]
         value = line[-1]
         sDict2[key] = value
+
 
 class Character:
 
@@ -63,7 +66,7 @@ class PlayVis:
         character_line = False
         ii = 0
         fl = 0
-        #print self.__dict__
+        # print self.__dict__
         for line in readfile:
             fl += 1
             # looks for the name
@@ -76,17 +79,18 @@ class PlayVis:
                 # we are in a characters line
                 character_line = True
             elif line == '':
-                if character_line == True:
+                if character_line:
                     self.character_lines[-1]['end'] = ii
                 character_line = False
-            # if the length of line line is not less than 3 and 
+            # if the length of line line is not less than 3 and
             # we are in a character line
             if not len(line) < 3 and character_line:
-                save += line # save the line
+                # save the line
+                save += line
             else:
                 tempLine = []
                 tempString = ""
-                tempString = line.translate(None,punctuation)
+                tempString = line.translate(None, punctuation)
                 tempString.lstrip()
                 oldLine = tempString.split()
                 tempString = tempString.lower()
@@ -95,20 +99,23 @@ class PlayVis:
                     if tempLine[0] == 'enter' or tempLine[0] == 're-enter':
                         for word in tempLine:
                             word = word.lower()
-                            if d.check(word) != True or oldLine[tempLine.index(word)][0].isupper:
+                            if (d.check(word) is not True or
+                                    oldLine[tempLine.index(word)][0].isupper):
                                 if word not in self.names:
-                                    #print word
+                                    # print word
                                     self.characters.append(Character(word))
                                     self.names[word] = self.characters[-1]
-                character_line = False # we are not in a character line
-                save += '************************************\n' # show that
+                # we are not in a character line
+                character_line = False
+                # show that
+                save += '************************************\n'
             ii += 1
         self.file_length = fl
         outfile.write(save)
 
-    #TODO
+    # TODO
     def find_characters(self):
-        #print self.character_lines
+        # print self.character_lines
         for line_num in self.character_lines:
             line_num['last_char'] = self.current_char
             changed = False
@@ -140,16 +147,17 @@ class PlayVis:
         Sent = sDict
         Sent2 = sDict2
         lst = []
-        #print self.character_lines
-        #print len(self.character_lines)
+        # print self.character_lines
+        # print len(self.character_lines)
         for line_group in self.character_lines:
             if 'end' not in line_group:
                 line_group['end'] = line_group['begin']
-            for line_num in range(line_group['begin'],line_group['end']): #get list of words for each line
+            # get list of words for each line
+            for line_num in range(line_group['begin'], line_group['end']):
                 line = linecache.getline(self.filename, line_num + 1)
                 line = line.strip()
                 line = line.lower()
-                line = line.translate(None,punctuation)
+                line = line.translate(None, punctuation)
                 lst = line.split()
                 if lst != []:
                     if lst[0] in self.names:
@@ -160,22 +168,19 @@ class PlayVis:
                     total += Sent[word]
                 if word in Sent2:
                     total += int(Sent2[word])
-            
-            #if total == 0: print lst, total
             line_group['sentiment'] = total
+
     def to_json(self):
         data = []
         for ii in range(1, self.file_length + 1):
-            #append all things that happend at line ii
-            changed = False
+            # append all things that happend at line ii
             for line_group in self.character_lines:
                 if line_group['begin'] == ii:
                     line_group['changed'] = True
-                    if line_group['last_char'] != None:
+                    if line_group['last_char'] is not None:
                         line_group['last_char'] = line_group['last_char'].name
                     data.append(line_group)
-                    changed == True
-                    break;
+                    break
         data_string = json.dumps(data)
         out = open('output.json', 'w')
         out.write(data_string)
@@ -188,10 +193,6 @@ if __name__ == '__main__':
     pv = PlayVis(sys.argv[1], 'output.txt')
     pv.format_paper()
     pv.find_characters()
-    #print pv.character_lines
-    #for character in pv.characteracters:
-        #if character.lines:
-           #print character.__dict__
     pv.sentiment_analysis()
     pv.to_json()
-    print 'characters: ', len( pv.characters)
+    print 'characters: ', len(pv.characters)
